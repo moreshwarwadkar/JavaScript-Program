226d68f62edc6b3343c3912985903f78ecdc0fc1
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-10-27 22:15:59 +0530

    Add files via upload

---
+++ 01_VARIABLES/01_ALL.js
@@ -0,0 +1,35 @@
+//!----- var Keyword -------
+
+// var a;   //!Declaration
+// a = 20;  //!-- Initialization
+
+// var a = 30;  //!-- Declaration + Initialization
+// var a;  //!-- Re-Declaration
+// a = 40;  //!-- Re-Initialization
+
+// var a = 50;  //!-- Re-declaration + Re-Initialization
+// console.log(a);
+
+
+
+//!----- let Keyword -----
+
+// let a;  //!-- Declaration
+// a = 30;  //!-- Initialization
+// let a = 50; //!-- Declaration + Initialization
+// let a;  //!-- Re-Declaration is Not Possible
+// a = 70; //!-- Re-Initialization
+// let a = 80;  //!-- Re-Declaration + Re-Initialization is Not Possible
+// console.log(a);
+
+
+
+//!----- const Keyword -----
+
+// const a;  //!-- Declaration is Not Possible
+// a = 20;  //!-- Initialization is Not Possible
+// const a = 40;  //!-- Declaration + Initialization is Possible
+// const a;  //!-- Re-Declaration is Not Possible
+// a = 50; //!-- Re-Initialization is Not Possible
+// const a = 50;  //!-- Re-Declaration + Re-Initialization is Not Possible
+// console.log(a);
