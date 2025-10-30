198472930fffab84e46d2ab86852efa01a9e1a9d
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-10-30 07:10:10 +0530

    Add files via upload

---
+++ 03_HOISTING/01_HOISTING.js
@@ -0,0 +1,30 @@
+/* ----- 1. Variable Hoisting ----- */
+
+// When we use var
+console.log(a); // It is Showing --> undefined
+var a = 10;
+
+// Similarly
+var a;
+console.log(a); // undefined
+a = 10;
+
+// When we use let and const
+console.log(b); // It is Showing -->  ReferenceError
+let b = 20;
+
+/* ----- 2. Function Hoisting ----- */
+
+greet(); // Output: Hello!
+
+function greet() {
+  console.log("Hello!");
+}
+
+// Here we Store the function then we call
+
+sayHi(); // TypeError
+
+var sayHi = function() {
+  console.log("Hi!");
+};
