e950b811a65266c3d5e1236f0220c1d0ae055195
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-10-30 07:00:23 +0530

    Enhance typeof operator examples and documentation
    Added comments and syntax explanation for typeof operator.

--- 02_OPERATORS/06_TYPE_OPERATOR/01_OP.js
+++ 02_OPERATORS/06_TYPE_OPERATOR/01_OP.js
@@ -1,5 +1,11 @@
 // Typeof Operator
 
+/*
+SYNTAX : 
+typeof(variableName);
+typeof value;
+*/
+
 console.log(typeof 123);          // "number"
 console.log(typeof "Hello");      // "string"
 console.log(typeof true);         // "boolean"
@@ -7,4 +13,5 @@ console.log(typeof undefined);    // "undefined"
 console.log(typeof null);         // "object"   (special case in JS)
 console.log(typeof {name: "Unik"}); // "object"
 console.log(typeof [1, 2, 3]);    // "object"   (array is a type of object)
+
 console.log(typeof function(){}); // "function"
