0fcd9c6a492fa224e7f30aa86f51a84f475fbd36
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-10-30 06:58:41 +0530

    Add syntax explanation for ternary operator
    Added comments explaining the syntax of the ternary operator.

--- 02_OPERATORS/05_TERNARY_OPERATOR/01_OP.js
+++ 02_OPERATORS/05_TERNARY_OPERATOR/01_OP.js
@@ -1,5 +1,11 @@
 // Ternary Opertor
 
+/*
+SYNTAX :
+condition ? expression_if_true : expression_if_false;
+*/
+
 let age = 18;
 let result = (age >= 18) ? "Adult" : "Minor";
+
 console.log(result);
