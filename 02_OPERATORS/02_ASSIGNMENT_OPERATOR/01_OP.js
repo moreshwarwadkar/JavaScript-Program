f21b7edaab2279e452f9b1238dc79cbfa2acbefe
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-10-30 06:53:56 +0530

    Enhance comments with assignment operator table
    Added a table summarizing assignment operators.

--- 02_OPERATORS/02_ASSIGNMENT_OPERATOR/01_OP.js
+++ 02_OPERATORS/02_ASSIGNMENT_OPERATOR/01_OP.js
@@ -1,5 +1,18 @@
 // Assignment Operator
 
+/*
+| **Operator** | **Name**            | **Example** | **Same As**  | **Result (if a = 10, b = 5)** |
+| ------------ | ------------------- | ----------- | ------------ | ----------------------------- |
+| `=`          | Simple Assignment   | `a = b`     | —            | `a = 5`                       |
+| `+=`         | Add and Assign      | `a += b`    | `a = a + b`  | `15`                          |
+| `-=`         | Subtract and Assign | `a -= b`    | `a = a - b`  | `5`                           |
+| `*=`         | Multiply and Assign | `a *= b`    | `a = a * b`  | `50`                          |
+| `/=`         | Divide and Assign   | `a /= b`    | `a = a / b`  | `2`                           |
+| `%=`         | Modulus and Assign  | `a %= b`    | `a = a % b`  | `0`                           |
+| `**=`        | Exponent and Assign | `a **= b`   | `a = a ** b` | `100000`                      |
+
+*/
+
 let x = 10;
 
 x += 5;   // x = 15
@@ -10,3 +23,4 @@ x %= 4;   // x = 2
 x **= 3;  // x = 8
 
 console.log(x);
+
