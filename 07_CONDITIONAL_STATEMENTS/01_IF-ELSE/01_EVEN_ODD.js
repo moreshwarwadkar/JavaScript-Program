a32445e411a6ec67b95b8eeb551590e9b1a27040
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-10-31 06:55:37 +0530

    Add files via upload

---
+++ 07_CONDITIONAL_STATEMENTS/01_IF-ELSE/01_EVEN_ODD.js
@@ -0,0 +1,8 @@
+let a = 5;
+
+if(a%2 == 0){
+    console.log('Number is Even');
+}
+else{
+    console.log('Number is Odd');
+}
---
+++ 07_CONDITIONAL_STATEMENTS/01_IF-ELSE/02_GREATEST_NUM.js
@@ -0,0 +1,13 @@
+let a = 15;
+let b = 10;
+let c = 20;
+
+if(a>b && a>c){
+    console.log('a is Greatest');
+}
+else if(b>a && b>c){
+    console.log('b is Greatest');
+}
+else{
+    console.log('c is Greatest');
+}
