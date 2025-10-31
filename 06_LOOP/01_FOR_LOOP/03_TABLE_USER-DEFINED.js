3e4bf8f8da62410c2fa22f62c53b68d0c7b307e6
Moreshwar Wadkar <157898321+moreshwarwadkar@users.noreply.github.com>
2025-10-31 06:55:05 +0530

    Add files via upload

---
+++ 06_LOOP/01_FOR_LOOP/03_TABLE_USER-DEFINED.js
@@ -0,0 +1,5 @@
+let num = Number(prompt('Enter Any Number:'))
+for(var i = 1; i <= 10; i++)
+{
+    console.log(num + " * "+i+" = "+num*i);
+}
---
+++ 06_LOOP/01_FOR_LOOP/04_FACTORIAL.js
@@ -0,0 +1,9 @@
+let fact = 1;
+let n = Number(prompt('Enter Any Number:'));
+
+for(let i=1; i<=n; i++)
+{
+    fact = fact*i;
+}
+
+console.log(fact);
